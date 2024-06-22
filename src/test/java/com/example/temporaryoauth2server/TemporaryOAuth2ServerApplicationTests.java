8d92b65b6d6c9cac18903f45263a3e6163ee81fa
HSH <han41562@gmail.com>
2024-06-22 16:16:47 +0900

    inital commit

---
+++ src/test/java/com/example/temporaryoauth2server/TemporaryOAuth2ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.temporaryoauth2server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemporaryOAuth2ServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
