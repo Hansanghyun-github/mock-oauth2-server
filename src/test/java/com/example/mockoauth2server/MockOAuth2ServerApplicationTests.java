88e7c75ddd8a897bf559e6a9337e621b6ff561ac
HSH <han41562@gmail.com>
2024-06-24 19:19:03 +0900

    fix: add test config file

--- src/test/java/com/example/mockoauth2server/MockOAuth2ServerApplicationTests.java
+++ src/test/java/com/example/mockoauth2server/MockOAuth2ServerApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.mockoauth2server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MockOAuth2ServerApplicationTests {
 
     @Test
